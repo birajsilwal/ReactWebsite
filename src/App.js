737cd03e3a42be7db9932568ffe7bbf00b19b190
biraj silwal <bsilwal@cs.unm.edu>
2020-08-14 21:28:10 -0600

    implemented google analytics

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ import Home from "./pages/Home";
 import Resume from "./pages/Resume";
 import { Route } from "react-router-dom";
 import TopHeader from "./components/Header";
+import ReactGA from "react-ga";
 
 function App() {
+
+  ReactGA.initialize('UA-142536391-1', {
+    
+    gaOptions: {
+      siteSpeedSampleRate: 100
+  }
+  });
+  ReactGA.pageview("/HomePage")
+
   return (
     <div className="App">
       <TopHeader />
