f90ef61a0e15b07ea09fb141e70614963ffc2c4b
Biraj Silwal <birajsilwal.ai@gmail.com>
2020-11-14 17:00:15 -0700

    resume updated nov 10

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -16,9 +16,9 @@ export default function Resume() {
   return (
     <div>
       <h3>Biraj Silwal's Resume</h3>
-      <p className={pInResume}>Last updated on 9th Sep 2020.</p>
+      <p className={pInResume}>Last updated on 14th Nov 2020.</p>
       <p className={ `${pInResume} pb-5`}>You can download my resume &nbsp;
-        <a className={downloadHere} href="https://birajsilwal.github.io/static/media/Resume%20-%20Biraj%20Silwal.fb648f60.pdf" target="_blank" rel="noopener noreferrer">
+        <a className={downloadHere} href="https://birajsilwal.github.io/static/media/resume.pdf" target="_blank" rel="noopener noreferrer">
         <Button variant="info">here</Button>{' '}
         </a>       
       </p>
